03b85e3b88a6e19fb7178370a3c39a921f9b364b
Veronika ArtIvanika <petrovskaya.veronika@gmail.com>
2023-06-16 15:20:09 +0300

    ПР13

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const userRoutes = require('./users');
+const cardRoutes = require('./cards');
+const { ERROR_NOT_FOUND } = require('../utils/status');
+
+router.use(userRoutes);
+router.use(cardRoutes);
+router.use('*', (req, res) => res.status(ERROR_NOT_FOUND).send({ message: 'Страница не найдена' }));
+module.exports = router;
