a92513a641add904d2803617fdfe74398b19df1e
Veronika ArtIvanika <petrovskaya.veronika@gmail.com>
2023-06-23 10:00:03 +0300

    pr14-t5

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use(auth);
 app.use(routes);
 app.use(errors());
 
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
   // если у ошибки нет статуса, выставляем 500
   const { statusCode = 500, message } = err;
   res
